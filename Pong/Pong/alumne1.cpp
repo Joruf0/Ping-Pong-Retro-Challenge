b11b8be449355a6d742554124c4111699a2bdf6f
Joruf0 <jordirubaufont@gmail.com>
2025-12-16 13:34:22 +0100

    Videojoc final

---
+++ Pong/Pong/alumne1.cpp
@@ -0,0 +1,104 @@
+#include "alumne1.h"
+#include <cmath>
+
+using namespace sf;
+
+void inicialitzarPilota(Pilota& pilota, float x, float y) {
+    pilota.radi = 10.0f;
+    pilota.forma.setRadius(pilota.radi);
+    pilota.forma.setPosition(Vector2f(x, y));
+    pilota.forma.setFillColor(Color::Black);
+    pilota.velocitat = Vector2f(300.0f, 200.0f);
+}
+
+void mourerPilota(Pilota& pilota, float deltaTime) {
+    pilota.forma.move(pilota.velocitat * deltaTime);
+}
+
+bool rebotPilota(Pilota& pilota, float amplada, float alcada) {
+    Vector2f posicio = pilota.forma.getPosition();
+    bool punt = false;
+
+    // Rebot superior i inferior
+    if (posicio.y <= 0 || posicio.y + pilota.radi * 2 >= alcada) {
+        pilota.velocitat.y = -pilota.velocitat.y;
+
+        // Corregir posicio per evitar que quedi enganxada
+        if (posicio.y <= 0) {
+            pilota.forma.setPosition(Vector2f(posicio.x, 0));
+        }
+        else {
+            pilota.forma.setPosition(Vector2f(posicio.x, alcada - pilota.radi * 2));
+        }
+    }
+
+    // Sortir per laterals (punt)
+    if (posicio.x <= 0 || posicio.x >= amplada) {
+        punt = true;
+    }
+
+    return punt;
+}
+
+void detectarColisio(Pilota& pilota, const Pala& palaEsquerra, const Pala& palaDreta) {
+    FloatRect limitsPilota = pilota.forma.getGlobalBounds();
+    FloatRect limitsEsquerra = palaEsquerra.forma.getGlobalBounds();
+    FloatRect limitsDreta = palaDreta.forma.getGlobalBounds();
+
+    // Colisio amb pala esquerra
+    if (limitsPilota.findIntersection(limitsEsquerra).has_value() && pilota.velocitat.x < 0) {
+        pilota.velocitat.x = -pilota.velocitat.x;
+        pilota.velocitat.x *= 1.15f;
+        pilota.forma.setPosition(Vector2f(limitsEsquerra.position.x + limitsEsquerra.size.x, pilota.forma.getPosition().y));
+    }
+
+    // Colisio amb pala dreta
+    if (limitsPilota.findIntersection(limitsDreta).has_value() && pilota.velocitat.x > 0) {
+        pilota.velocitat.x = -pilota.velocitat.x;
+        pilota.velocitat.x *= 1.15f;
+        pilota.forma.setPosition(Vector2f(limitsDreta.position.x - pilota.radi * 2, pilota.forma.getPosition().y));
+    }
+}
+
+void inicialitzarPala(Pala& pala, float x, float y, float amplada, float alcada) {
+    pala.forma.setSize(Vector2f(amplada, alcada));
+    pala.forma.setPosition(Vector2f(x, y));
+    pala.forma.setFillColor(Color::White);
+    pala.velocitat = 800.0f;
+}
+
+void mourerPalaEsquerra(Pala& pala, float deltaTime, float limitSuperior, float limitInferior) {
+    if (Keyboard::isKeyPressed(Keyboard::Key::W)) {
+        pala.forma.move(Vector2f(0, -pala.velocitat * deltaTime));
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
+        pala.forma.move(Vector2f(0, pala.velocitat * deltaTime));
+    }
+
+    // Limitar moviment
+    Vector2f posicio = pala.forma.getPosition();
+    if (posicio.y < limitSuperior) {
+        pala.forma.setPosition(Vector2f(posicio.x, limitSuperior));
+    }
+    if (posicio.y + pala.forma.getSize().y > limitInferior) {
+        pala.forma.setPosition(Vector2f(posicio.x, limitInferior - pala.forma.getSize().y));
+    }
+}
+
+void mourerPalaDreta(Pala& pala, float deltaTime, float limitSuperior, float limitInferior) {
+    if (Keyboard::isKeyPressed(Keyboard::Key::Up)) {
+        pala.forma.move(Vector2f(0, -pala.velocitat * deltaTime));
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::Down)) {
+        pala.forma.move(Vector2f(0, pala.velocitat * deltaTime));
+    }
+
+    // Limitar moviment
+    Vector2f posicio = pala.forma.getPosition();
+    if (posicio.y < limitSuperior) {
+        pala.forma.setPosition(Vector2f(posicio.x, limitSuperior));
+    }
+    if (posicio.y + pala.forma.getSize().y > limitInferior) {
+        pala.forma.setPosition(Vector2f(posicio.x, limitInferior - pala.forma.getSize().y));
+    }
+}
---
+++ Pong/Pong/main.cpp
@@ -0,0 +1,109 @@
+#include <SFML/Graphics.hpp>
+#include "alumne1.h"
+#include "alumne2.h"
+
+using namespace sf;
+
+int main() {
+    // Constants del joc
+    const float AMPLADA = 800.0f;
+    const float ALCADA = 600.0f;
+
+    // Crear finestra
+    RenderWindow finestra;
+    inicialitzarJoc(finestra);
+
+    // Inicialitzar elements del joc
+    Pilota pilota;
+    inicialitzarPilota(pilota, AMPLADA / 2, ALCADA / 2);
+
+    Pala palaEsquerra;
+    inicialitzarPala(palaEsquerra, 30, ALCADA / 2 - 50, 15, 100);
+
+    Pala palaDreta;
+    inicialitzarPala(palaDreta, AMPLADA - 45, ALCADA / 2 - 50, 15, 100);
+
+    Marcador marcador;
+    if (!inicialitzarMarcador(marcador)) {
+        return -1; // Error carregant la font
+    }
+
+    // Variables de control
+    Clock rellotge;
+    bool jocAcabat = false;
+    int guanyador = 0;
+
+    // Bucle principal
+    while (finestra.isOpen()) {
+        float deltaTime = rellotge.restart().asSeconds();
+
+        // Gestio d'esdeveniments
+        while (auto event = finestra.pollEvent()) {
+            if (event->is<Event::Closed>()) {
+                finestra.close();
+            }
+
+            // Reiniciar joc si s'ha acabat
+            if (jocAcabat && event->is<Event::KeyPressed>()) {
+                const auto& keyEvent = event->getIf<Event::KeyPressed>();
+                if (keyEvent && keyEvent->code == Keyboard::Key::Space) {
+                    marcador.puntsEsquerra = 0;
+                    marcador.puntsDreta = 0;
+                    jocAcabat = false;
+                    guanyador = 0;
+                    inicialitzarPilota(pilota, AMPLADA / 2, ALCADA / 2);
+                }
+            }
+        }
+
+        if (!jocAcabat) {
+            // Moure pales
+            mourerPalaEsquerra(palaEsquerra, deltaTime, 0, ALCADA);
+            mourerPalaDreta(palaDreta, deltaTime, 0, ALCADA);
+
+            // Moure pilota
+            mourerPilota(pilota, deltaTime);
+
+            // Detectar colisions amb pales
+            detectarColisio(pilota, palaEsquerra, palaDreta);
+
+            // Detectar rebots i punts
+            bool hiHaPunt = rebotPilota(pilota, AMPLADA, ALCADA);
+
+            if (hiHaPunt) {
+                Vector2f posicio = pilota.forma.getPosition();
+
+                if (posicio.x <= 0) {
+                    // Punt per al jugador dreta
+                    actualitzarMarcador(marcador, 2);
+                    reiniciarPilota(pilota, AMPLADA, ALCADA, 2);
+                }
+                else {
+                    // Punt per al jugador esquerra
+                    actualitzarMarcador(marcador, 1);
+                    reiniciarPilota(pilota, AMPLADA, ALCADA, 1);
+                }
+            }
+
+            // Comprovar si el joc ha acabat
+            comprovarFinal(marcador, jocAcabat, guanyador);
+        }
+
+        // Dibuixar tot
+        finestra.clear(Color::Black);
+
+        if (!jocAcabat) {
+            finestra.draw(palaEsquerra.forma);
+            finestra.draw(palaDreta.forma);
+            finestra.draw(pilota.forma);
+        }
+        else {
+            mostrarFinal(finestra, guanyador, marcador.font);
+        }
+
+        mostrarMarcador(finestra, marcador);
+        finestra.display();
+    }
+
+    return 0;
+}
